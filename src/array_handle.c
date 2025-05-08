61a6803486d597fbf164dae69473a01d59ffb6ea
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-05-08 18:07:40 +0300

    Знакомство с getline()

--- src/array_handle.c
+++ src/array_handle.c
@@ -3,47 +3,57 @@
 #include <assert.h>
 #include <malloc.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <string.h>
 
 constexpr size_t size_int = sizeof(int);
-constexpr int STEP = 32;
+
+// constexpr int STEP = 32;
 
 int get_array(int ** arr)
 {
 	if (!arr)
 		return -1;
 
-	int len = 0, capacity = 0;
+	ssize_t str_len = 0; 
+	size_t str_capacity = 0;
 	*arr = nullptr; // Зануление указателя перед началом работы
 
-	do {
-		assert(capacity >= 0);
-		assert(len >= 0);
-		int tmp;
-		printf("Введите %d-ое число: ", len + 1);
+	char * line = NULL;
+	if (-1 == (str_len = getline(&line, &str_capacity, stdin))) {
+		free(line);
+		return -1;
+	}
+	return 0;
 
-		/*Если ввод завершён, возврат числа введённых элементов*/
-		if (1 != scanf("%d", &tmp))
-			return len;
-
-		/*Если место в выделенной памяти заканчивается*/
-		if (capacity < len + 1) {
-			int * ptr = (int *)realloc(
-				*arr, size_int * (size_t)(capacity += STEP));
-			if (!ptr) {
-				/*Освобождение памяти старого указателя*/
-				free(*arr);
-				/*Возврат кода ошибки при нехватке памяти*/
-				return -1;
-			}
-			*arr = ptr;
-		}
-
-		assert(*arr != nullptr);
-		/*Перезапись крайнего элемента*/
-		(*arr)[len++] = tmp;
-
-	} while (true);
+	// do {
+	// 	assert(capacity >= 0);
+	// 	assert(len >= 0);
+	// 	int tmp;
+	// 	printf("Введите %d-ое число: ", len + 1);
+	//
+	// 	/*Если ввод завершён, возврат числа введённых элементов*/
+	// 	if (1 != scanf("%d", &tmp))
+	// 		return len;
+	//
+	// 	/*Если место в выделенной памяти заканчивается*/
+	// 	if (capacity < len + 1) {
+	// 		int * ptr = (int *)realloc(
+	// 			*arr, size_int * (size_t)(capacity += STEP));
+	// 		if (!ptr) {
+	// 			/*Освобождение памяти старого указателя*/
+	// 			free(*arr);
+	// 			/*Возврат кода ошибки при нехватке памяти*/
+	// 			return -1;
+	// 		}
+	// 		*arr = ptr;
+	// 	}
+	//
+	// 	assert(*arr != nullptr);
+	// 	/*Перезапись крайнего элемента*/
+	// 	(*arr)[len++] = tmp;
+	//
+	// } while (true);
 }
 
 void print_array(int const * arr, int len)
@@ -60,14 +70,14 @@ static enum ArmstongResult armstong_wrap(int n)
 	return check_armstrong(n, &dumb);
 }
 
-static int sum_of_digits (int num)
+static int sum_of_digits(int num)
 {
 	int count = count_digits(num);
 	int sum = get_nth_digit(num, 0);
 	assert(count >= 0);
 
 	for (int i = 1; i < count; ++i)
-		sum += get_nth_digit(num,i);
+		sum += get_nth_digit(num, i);
 
 	assert(sum >= 0);
 	return sum;
